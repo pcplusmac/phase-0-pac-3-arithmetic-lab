11854fda1e5de3c7ce69f0a4fdcea2df32babdfc
el-sam <samurai.h@yahoo.com>
2023-09-12 18:43:56 +0800

    add 2 parameter to add function

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+var a, b; 
 //define function add()
 function add(a,b){
     return a+b;
+}
+
+// Define function substract
+function subtract(){
+    return a-b;
+}
+
+// Define multiply()
+function multiply(){
+    return a*b;
+}
+
+// Define function divide()
+function divide() {
+    return a/b;
 }
